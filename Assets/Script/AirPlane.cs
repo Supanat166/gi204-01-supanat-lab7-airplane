d37d0878fa7209660afbb4e4cad9d5e1e09be9c2
Supanat166 <supanat.siem@bumail.net>
2025-03-06 16:29:26 +0700

    Add roll

--- Assets/Script/AirPlane.cs
+++ Assets/Script/AirPlane.cs
@@ -8,6 +8,10 @@ public class NewMonoBehaviourScript : MonoBehaviour
     [SerializeField] private float liftBooster = 0.5f;
     [SerializeField] private float drag = 0.001f;
     [SerializeField] private float angularDrag = 0.001f;
+
+    [SerializeField] private float yawPower = 50f;
+    [SerializeField] private float pitchPower = 50f;
+    [SerializeField] private float rollPower = 30f;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -25,6 +29,18 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
         Vector3 lift = Vector3.Project(rb.linearVelocity, transform.forward);
         rb.AddForce(transform.up * lift.magnitude * liftBooster);
+
+        rb.linearDamping = rb.linearVelocity.magnitude * drag;
+        rb.angularDamping = rb.linearVelocity.magnitude * angularDrag;
+
+        float yaw = Input.GetAxis("Horizontal") * yawPower;
+        float pitch = Input.GetAxis("Vertical") * pitchPower;
+        float roll = Input.GetAxis("Roll") * rollPower;
+        
+        rb.AddTorque(transform.up * yaw);
+        rb.AddTorque(transform.right * pitch);
+        rb.AddTorque(transform.forward * roll);
+
 
     }
     
